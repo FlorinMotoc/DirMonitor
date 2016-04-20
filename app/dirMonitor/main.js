9cd860988f1ea2c1e19520681de8aec6b61e9322
Florin Motoc <dev@mflorin.com>
2016-04-20 12:23:19 +0300

    - Added jquery to package.json - Changed dirMonitor/main.js to use jquery. - Changed app.html button to use Class instead of ID (for multiple watchers), and also to use a data-what (=start or =stop) for starting/stopping purpose.

--- app/dirMonitor/main.js
+++ app/dirMonitor/main.js
@@ -1,31 +1,41 @@
 
 import { remote } from 'electron'; // native electron module
-
 import { dirMonitor } from './dirMonitor';
 
+window.$ = window.jQuery = require('jquery');
+
 var dialog = remote.dialog;
+
 
 export var dirMonitorMain = new function () {
     var thisClass = this;
 
     this.start = function () {
-        thisClass.onDocumentReady();
-       
     }
 
-    this.onDocumentReady = function () {
-        document.addEventListener('DOMContentLoaded', function () {
-            document.getElementById('btnClick').onclick = function() {thisClass.abc()};
+    $(function() {
+        // On Button Click, show the Chose Directory Dialog
+        $('.container').on('click', '.btnClick', function () {
+            var $btn = $(this);
+            var what = $btn.data('what');
+            if (what == 'start') { // START
+                // Display File Browser Dialog, wait for the user to select one directory, continue after
+                var dirPath = thisClass.openBrowseDialog_ReturnPathOrFalse();
+                if ( dirPath ) {
+                    dirMonitor.setDir( dirPath );
+                    dirMonitor.start();
+                } else {
+                    console.info('no dir selected');
+                }
+            } else {
+                // STOP
+            }
         });
-    }
+    });
 
-    this.abc = function () {
+    this.openBrowseDialog_ReturnPathOrFalse = function () {
         var dirChoser = dialog.showOpenDialog({ properties: [ 'openDirectory' ]});
-        if (dirChoser[0]) {
-            dirMonitor.setDir( dirChoser[0] );
-            // dirMonitor.setIgnored(/\\/);
-            dirMonitor.start();
-        }
+        return ( dirChoser && dirChoser[0] ) ? dirChoser[0] : false ;
     }
 
 }
